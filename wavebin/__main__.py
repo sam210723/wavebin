99acafededfd0048180c5770958e4f2d0649a823
Sam <sam210723@hotmail.com>
2019-11-16 14:44:11 +1000

    Parse arguments

--- wavebin/__main__.py
+++ wavebin/__main__.py
@@ -1,5 +1,21 @@
+from argparse import ArgumentParser
+
 def init():
-    return    
+    args = parse_args()
+    print(args)
+
+
+def parse_args():
+    """
+    Parses command line arguments
+    """
+
+    argp = ArgumentParser()
+    argp.prog = "wavebin"
+    argp.description = "Keysight/Agilent oscilloscope waveform file converter"
+    argp.add_argument("BIN", action="store", help="Path to waveform file (.bin)")
+
+    return argp.parse_args()
 
 
 try:
