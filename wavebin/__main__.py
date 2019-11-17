b0e598e9d6bb8138ef8e3ed8f351a96566036ac0
Sam <sam210723@hotmail.com>
2019-11-18 09:18:25 +1000

    Separate print functions

--- wavebin/__main__.py
+++ wavebin/__main__.py
@@ -22,49 +22,11 @@ def init():
     
     # Parse file header
     file_header = parse_file_header(bin_bytes[:12])
+    print_file_header(file_header)
 
     # Parse Waveform Header
     wave_header = parse_wave_header(bin_bytes[12:])
-
-
-def parse_wave_header(data):
-    """
-    Parses waveform header into Named Tuple
-    """
-    
-    # Get header length
-    header_len = data[0]
-
-    # Unpack header fields
-    fields = struct.unpack("5if3d2i16s16s24s16sdI", data[:header_len])
-    wave_header = tuples.WaveHeader(*fields)
-
-    # Print waveform info
-    print("Waveform {}:".format(wave_header.label.decode().rstrip('\0')))
-    print("  - Wave Type:           {}".format(enums.WaveType(wave_header.wave_type).name))
-    print("  - Wave Buffers:        {}".format(wave_header.buffers))
-    print("  - Sample Points:       {}".format(wave_header.points))
-    print("  - Average Count:       {}".format(wave_header.count))
-    rng = mg(wave_header.x_d_range, unit="s", ounit="us")
-    print("  - X Display Range:     {}".format(rng))
-    dorigin = mg(wave_header.x_d_origin, unit="s", ounit="us")
-    print("  - X Display Origin:    {}".format(dorigin))
-    increment = mg(wave_header.x_increment, unit="s", ounit="ns")
-    print("  - X Increment:         {}".format(increment))
-    origin = mg(wave_header.x_origin, unit="s", ounit="us")
-    print("  - X Origin:            {}".format(origin))
-    print("  - X Units:             {}".format(enums.Units(wave_header.x_units).name))
-    print("  - Y Units:             {}".format(enums.Units(wave_header.y_units).name))
-    print("  - Date:                {}".format(wave_header.date.decode()))
-    print("  - Time:                {}".format(wave_header.time.decode()))
-    frame = wave_header.frame.decode().split(":")
-    print("  - Frame Type:          {}".format(frame[0]))
-    print("  - Frame Serial:        {}".format(frame[1]))
-    print("  - Waveform Label:      {}".format(wave_header.label.decode()))
-    print("  - Time Tags:           {}".format(wave_header.time_tags))
-    print("  - Segment Number:      {}".format(wave_header.segment))
-
-    return wave_header
+    print_wave_header(wave_header)
 
 
 def parse_file_header(data):
@@ -80,13 +42,54 @@ def parse_file_header(data):
     if (file_header.signature.decode() != "AG"):
         print("UNEXPECTED FILE SIGNATURE\nExiting...\n")
         exit(1)
-    
-    # Print file info
-    size = round(file_header.size / 1024, 2)
-    print("File Size:           {} KB".format(size))
-    print("Waveforms:           {}\n".format(file_header.waveforms))
 
     return file_header
+
+def parse_wave_header(data):
+    """
+    Parses waveform header into Named Tuple
+    """
+    
+    # Get header length
+    header_len = data[0]
+
+    # Unpack header fields
+    fields = struct.unpack("5if3d2i16s16s24s16sdI", data[:header_len])
+    wave_header = tuples.WaveHeader(*fields)
+
+    return wave_header
+
+
+def print_file_header(header):
+    # Print file info
+    size = round(header.size / 1024, 2)
+    print("File Size:           {} KB".format(size))
+    print("Waveforms:           {}\n".format(header.waveforms))
+
+def print_wave_header(header):
+    print("Waveform {}:".format(header.label.decode().rstrip('\0')))
+    print("  - Wave Type:           {}".format(enums.WaveType(header.wave_type).name))
+    print("  - Wave Buffers:        {}".format(header.buffers))
+    print("  - Sample Points:       {}".format(header.points))
+    print("  - Average Count:       {}".format(header.count))
+    rng = mg(header.x_d_range, unit="s", ounit="us")
+    print("  - X Display Range:     {}".format(rng))
+    dorigin = mg(header.x_d_origin, unit="s", ounit="us")
+    print("  - X Display Origin:    {}".format(dorigin))
+    increment = mg(header.x_increment, unit="s", ounit="ns")
+    print("  - X Increment:         {}".format(increment))
+    origin = mg(header.x_origin, unit="s", ounit="us")
+    print("  - X Origin:            {}".format(origin))
+    print("  - X Units:             {}".format(enums.Units(header.x_units).name))
+    print("  - Y Units:             {}".format(enums.Units(header.y_units).name))
+    print("  - Date:                {}".format(header.date.decode()))
+    print("  - Time:                {}".format(header.time.decode()))
+    frame = header.frame.decode().split(":")
+    print("  - Frame Type:          {}".format(frame[0]))
+    print("  - Frame Serial:        {}".format(frame[1]))
+    print("  - Waveform Label:      {}".format(header.label.decode()))
+    print("  - Time Tags:           {}".format(header.time_tags))
+    print("  - Segment Number:      {}".format(header.segment))
 
 
 def parse_args():
