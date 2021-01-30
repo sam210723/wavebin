2eb02bfbe68b43caee228df3d20ee66220018dc5
Sam <sam210723@hotmail.com>
2021-01-30 22:55:44 +1000

    Extra PyPI classifiers

--- setup.py
+++ setup.py
@@ -20,12 +20,19 @@ setuptools.setup(
         'pyqtgraph'
     ],
     classifiers=[
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Environment :: Console",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Scientific/Engineering :: Visualization",
-        "Topic :: Utilities"
+        "Programming Language :: Python :: 3.9"
     ]
 )
