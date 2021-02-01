79229fc5c37a7c66481b66bc3d219ea11ee0f67b
Sam <sam210723@hotmail.com>
2021-02-01 10:17:20 +1000

    Set y-axis label

--- wavebin/plot.py
+++ wavebin/plot.py
@@ -5,9 +5,11 @@ https://github.com/sam210723/wavebin
 Waveform capture viewer for Keysight oscilloscopes.
 """
 
+from enum import Enum
 import numpy as np
 from pyqtgraph import PlotWidget
 import pyqtgraph as pg
+
 
 class QtPlot(PlotWidget):
     def __init__(self, config):
@@ -39,7 +41,7 @@ class QtPlot(PlotWidget):
         self.clear()
 
         # Loop through waveforms and render traces
-        for i, w in enumerate(self.waveforms): 
+        for i, w in enumerate(self.waveforms):
             # Generate X points
             start = w['header'].x_d_origin
             stop = w['header'].x_d_origin + w['header'].x_d_range
@@ -53,7 +55,42 @@ class QtPlot(PlotWidget):
                     width=self.config['line_width']
                 )
             )
+        
+        # Set left Y axis label
+        self.setLabel(
+            'left',
+            Units(self.waveforms[0]['header'].y_units).name,
+            units=UnitAbbr(self.waveforms[0]['header'].y_units).name
+        )
 
 
     def log(self, msg):
         if self.config['verbose']: print(msg)
+
+
+class Units(Enum):
+    """
+    Waveform units
+    """
+
+    UNKNOWN     = 0
+    Volts       = 1
+    Seconds     = 2
+    Constant    = 3
+    Amps        = 4
+    Decibels    = 5
+    Hertz       = 6
+
+
+class UnitAbbr(Enum):
+    """
+    Waveform unit abbreviations
+    """
+
+    UNKNOWN     = 0
+    V           = 1
+    s           = 2
+    C           = 3
+    A           = 4
+    dB          = 5
+    Hz          = 6
