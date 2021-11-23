04129a958dacdacd413a95c7e7da46a49dc4dc29
Sam <sam210723@hotmail.com>
2021-11-23 20:04:13 +1000

    Vispy testing

--- wavebin/interface/plot_vispy.py
+++ wavebin/interface/plot_vispy.py
@@ -5,10 +5,15 @@ https://github.com/sam210723/wavebin
 Oscilloscope waveform capture viewer
 """
 
+from vispy import scene
+import numpy as np
+
 from wavebin.vendor import Channel
 
+# https://vispy.org/gallery/scene/axes_plot.html
 
-class WaveformPlot():
+
+class WaveformPlot(scene.SceneCanvas):
     """
     Waveform plotting widget using vispy backend
     """
@@ -24,9 +29,20 @@ class WaveformPlot():
         """
 
         # Initialise parent class
-        super(WaveformPlot, self).__init__()
+        super(WaveformPlot, self).__init__(keys="interactive")
 
         # Set globals
+        self.unfreeze()
         self.config = config
         self.waveform = waveform
         self.colour = colour
+
+        grid = self.central_widget.add_grid(margin=10)
+        grid.spacing = 0
+
+        view = grid.add_view(row=1, col=1, border_color='white')
+        self.waveform._trace = np.swapaxes(self.waveform.trace, 0, 1)
+        line = scene.Line(self.waveform.trace, parent=view.scene)
+
+        view.camera = 'panzoom'
+        view.camera.set_range()
