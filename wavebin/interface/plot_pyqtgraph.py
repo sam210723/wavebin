3913822fa084e9450ca1bcb13b0d1ae5a44598ea
Sam <sam210723@hotmail.com>
2021-12-06 13:56:21 +1000

    Show plots for each axis with Units from Vendor class

--- wavebin/interface/plot_pyqtgraph.py
+++ wavebin/interface/plot_pyqtgraph.py
@@ -9,7 +9,7 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph import GraphicsLayoutWidget, PlotItem, ViewBox, AxisItem
 
-from wavebin.vendor import Vendor
+from wavebin.vendor import Vendor, Unit, UnitAbbr
 
 # https://pyqtgraph.readthedocs.io/en/latest/plotting.html#organization-of-plotting-classes
 
@@ -45,9 +45,11 @@ class WaveformPlot(GraphicsLayoutWidget):
             (255, 0, 0),
             (255, 165, 0)
         ]
+        self.axis_pen = pg.mkPen(color="#969696", width=1)
 
         # Loop through waveform channels
         for i, c in enumerate(self.waveform.channels):
+            last = i == len(self.waveform.channels) - 1
             # Create plot object
             c.plot = PlotItem()
             view = c.plot.getViewBox()
@@ -56,11 +58,11 @@ class WaveformPlot(GraphicsLayoutWidget):
             view.setBorder(None)
             view.setMouseMode(ViewBox.PanMode)
             view.setBorder("#969696")
-            c.plot.setMenuEnabled(enableMenu=False)
+            c.plot.setMenuEnabled(False)
             c.plot.setMouseEnabled(x=True, y=False)
             c.plot.hideButtons()
 
-            # Setup plot limits and axes
+            # Setup plot limits
             c.plot.setYRange(np.min(c.trace[1]), np.max(c.trace[1]), padding=0.1)
             c.plot.setLimits(
                 xMin=np.min(c.trace[0]),
@@ -68,21 +70,26 @@ class WaveformPlot(GraphicsLayoutWidget):
                 yMin=np.min(c.trace[1]),
                 yMax=np.max(c.trace[1])
             )
-            c.plot.showAxes((False, True, True, False))
-            c.plot.getAxis('right').setWidth(w=40)
-            c.plot.getAxis('right').setLabel(text="Volts", units='V')
-            c.plot.getAxis('right').setPen(pg.mkPen(color="#969696", width=1))
-            #TODO: Set y axis units
+
+            # Setup plot axes
+            c.plot.showAxes((True, False, False, True))
+            c.plot.getAxis('bottom').setHeight(40)
+            c.plot.getAxis('bottom').setLabel(text=c.x_unit.name, units=UnitAbbr(c.x_unit.value).name)
+            c.plot.getAxis('bottom').setPen(self.axis_pen)
+            c.plot.getAxis('left').setWidth(40)
+            c.plot.getAxis('left').setLabel(text=c.y_unit.name, units=UnitAbbr(c.y_unit.value).name)
+            c.plot.getAxis('left').setPen(self.axis_pen)
 
             #FIXME: showGrid() causes zoom to center on X origin rather than mouse location
             #FIXME: See pyqtgraph/pyqtgraph #1937, #1975, #2034 and #2057
             #FIXME: This is fixed in the dev version of pyqtgraph, not yet released
             #FIXME: setuptools can not install deps from GitHub when package is being installed from PyPI
-            c.plot.showGrid(x=True, y=True, alpha=0.5)
+            c.plot.showGrid(x=True, y=True, alpha=0.75)
 
             # Subsample large waveform captures
             limit = 250000
             if c.points > limit:
+                print(f"Subsampling channel with more than {limit/1000:.0f}k points")
                 c.plot.setDownsampling(
                     ds=int(c.points/limit),
                     auto=False,
@@ -94,22 +101,13 @@ class WaveformPlot(GraphicsLayoutWidget):
                 c.trace[0],
                 c.trace[1],
                 clear=True,
-                #skipFiniteCheck=True,
+                skipFiniteCheck=True,
                 pen=pg.mkPen(
                     self.colours[i],
                     width=2     # See pyqtgraph/pyqtgraph #533
                 )
             )
 
-            # Link x-axis of all views
+            # Link views and add to layout
             if i != 0: c.plot.setXLink(self.waveform.channels[0].plot.getViewBox())
-
-            # Add plot to layout
-            self.addItem(c.plot, row=i+1, col=0, rowspan=1, colspan=1)
-        
-        # Create time-axis (x)
-        self.xaxis = AxisItem(orientation="top")
-        self.xaxis.linkToView(self.waveform.channels[0].plot.getViewBox())
-        self.xaxis.setLabel(text="Time", units='s')
-        self.xaxis.enableAutoSIPrefix(True)
-        self.addItem(self.xaxis, row=0, col=0)
+            self.addItem(c.plot, row=i, col=0, rowspan=1, colspan=1)
