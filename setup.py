23f6cb3c5802e81b46b73c7354e336fac86d3770
Sam <sam210723@hotmail.com>
2022-05-08 17:56:54 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         'numpy>=1.21.0',
         'pyqt5>=5.15.0',
         'pyopengl>=3.1.6',
-        'pyopengl-accelerate>=3.1.6'
+        'pyopengl-accelerate>=3.1.6',
         'pyqtgraph>=0.12.4',
         'qtawesome>=1.1.0',
         'requests>=2.24.0'
